12ff0013f8e41937154fab9c0cf03dfd0849f680
EgorZenzin <124917529+EgorZenzin@users.noreply.github.com>
2023-06-23 22:11:04 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,28 +4,29 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double e = 1e-9;
-        if (-e < a && a < e) throw new ArgumentException();
+        double eps = 1e-9;
+         if (-eps < a && a < eps) throw new System.ArgumentException();
         
         if ( double.IsNaN(a)  double.IsInfinity(a)  double.IsNaN(b)  double.IsInfinity(b)  double.IsNaN(c) || double.IsInfinity(c)) throw new System.ArgumentException();
         
         b = b / a;
         c = c / a;
-        double discr = b * b - 4 * c;
+        double d = b * b - 4 * c;
         double[] zeroRoots = new double[] { };
-        if (discr <= -e) return zeroRoots;
-        else if (-e < discr && discr < e)
+        if (d <= -eps) return zeroRoots;
+        else if (-eps < d && d < eps)
         {
             double[] Roots1 = new double[1];
-            oneRoots[0] = -(b) / 2;
+            Roots1[0] = -(b) / 2;
             return Roots1;
         }
         else
         {
             double[] Roots2 = new double[2];
-            Roots2[0] = -(b + Math.Sign(b) * Math.Sqrt(discr)) / 2;
+            Roots2[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
             Roots2[1] = c / Roots2[0];
             return Roots2;
         }
+
     }
-}     
+}
