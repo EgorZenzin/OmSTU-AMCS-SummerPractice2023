a055ed9f6eb93a97f6e68a9a34f7e35ae1199b17
EgorZenzin <124917529+EgorZenzin@users.noreply.github.com>
2023-06-23 21:05:38 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,9 +1,32 @@
+using System;
 ﻿namespace SquareEquationLib;
-
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
+        if (a==0 || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        {
+            throw new ArgumentException("Ошибка");
+        }
+        double x1;
+        double x2;  
+        double[] array = new double[2];
+        double d = b * b - 4 * a * c;
+        if (d < 0)
+        {
+            array = new double[0];
+        }
+        if (Math.Abs(d)<double.Epsilon)
+        {
+             x1 =2*c/-b;
+             array = new double[] { x1 };
+        }
+        if (d>0)
+        {
+            x1 = -(b+Math.Sign(b)*Math.Sqrt(d))/2;
+            x2 = c / x1;
+            array = new double[] { x1, x2 };
+        }
+        return array;
     }
 }
