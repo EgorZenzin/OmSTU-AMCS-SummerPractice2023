645b3b81c1d12121bcc31c0d1b869e7e8cadcaae
EgorZenzin <124917529+EgorZenzin@users.noreply.github.com>
2023-06-23 22:03:38 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,34 +1,38 @@
-namespace SquareEquationLib;
-public static class SquareEquation
+amespace SquareEquationLib
 {
-    public static double[] Solve(double a, double b, double c)
+    public static class SquareEquation
     {
-        const double epsilon = 1e-9;
-        if (Math.Abs(a) < epsilon)
+        public static double[] Solve(double a, double b, double c)
         {
-            throw new ArgumentException("Coefficient 'a' should not be zero.");
-        }
-        if (!double.IsFinite(a) || !double.IsFinite(b) || !double.IsFinite(c))
-        {
-            throw new ArgumentException("Coefficients should be finite numbers.");
-        }
-        b /= a;
-        c /= a;
-        double discriminant = b * b - 4 * c;
-        if (Math.Abs(discriminant) < epsilon)
-        {
-            return new double[] { -b / 2 };
-        }
-        else if (discriminant < -epsilon)
-        {
-            return new double[0];
-        }
-        else
-        {
-            double sqrtDiscriminant = Math.Sqrt(discriminant);
-            double root1 = (-b + Math.Sign(b) * sqrtDiscriminant) / 2;
-            double root2 = c / root1;
-            return new double[] { root1, root2 };
+            double epsilon = double.Epsilon;
+            if (Math.Abs(a) < epsilon)
+            {
+                throw new ArgumentException("The coefficient 'a' should not be zero.");
+            }
+            if (double.IsNaN(a) || double.IsInfinity(a) || double.IsNaN(b) || double.IsInfinity(b) || double.IsNaN(c) || double.IsInfinity(c))
+            {
+                throw new ArgumentException("The coefficients should be finite numbers.");
+            }
+            b /= a;
+            c /= a;
+            double discriminant = b * b - 4 * c;
+            double[] roots;
+            if (discriminant < -epsilon)
+            {
+                roots = new double[0];
+            }
+            else if (Math.Abs(discriminant) < epsilon)
+            {
+                roots = new double[] { -b / 2 };
+            }
+            else
+            {
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double root1 = -(b + Math.Sign(b) * sqrtDiscriminant) / 2;
+                double root2 = c / root1;
+                roots = new double[] { root1, root2 };
+            }
+            return roots;
         }
     }
-}
+}  
