6dc583e11dd4f8b4771397ee955759c55ce8d811
Egor <zenzin48@mail.ru>
2023-06-26 13:54:24 +0600

    Praktika3

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,28 +4,29 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double e = 1e-9;
-        if (-e < a && a < e) throw new System.ArgumentException();
-        
-        if ( new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity)) throw new System.ArgumentException();
-        
+        double eps = 1e-9;
+        if (-eps < a && a < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        {
+            throw new ArgumentException();
+        }
         b = b / a;
         c = c / a;
         double d = b * b - 4 * c;
         double[] zeroRoots = new double[] { };
-        if (d <= -e) return zeroRoots;
-        else if (-e < d && d < e)
-        {
-            double[] Roots1 = new double[1];
-            Roots1[0] = -(b) / 2;
-            return Roots1;
+        if (d <= -eps) return zeroRoots;
+        else if (-eps < d & d < eps)
+        { 
+            double[] oneRoots = new double[1];
+            oneRoots[0] = -(b) / 2;
+            return oneRoots;
         }
         else
         {
-            double[] Roots2 = new double[2];
-            Roots2[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
-            Roots2[1] = c / Roots2[0];
-            return Roots2;
+            double[] twoRoots = new double[2];
+            twoRoots[0] = -(b + Math.Sign(b) * Math.Sqrt(d)) / 2;
+            twoRoots[1] = c / twoRoots[0];
+            return twoRoots;
         }
+
     }
 }
